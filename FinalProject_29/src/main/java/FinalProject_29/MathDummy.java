bc94ce948fdb8112d19e30fed287ab8352d1c60e
Ren <ter.renata@gmail.com>
2019-11-11 16:31:12 -0500

    adding

--- FinalProject_29/src/main/java/FinalProject_29/MathDummy.java
+++ FinalProject_29/src/main/java/FinalProject_29/MathDummy.java
@@ -1,10 +1,12 @@
+package FinalProject_29;
 
-public class SUT {
+
+public class MathDummy {
 
 	public static int Math(int var1, int var2) {
-		int w = var1 / var2;
-		int q = 1 / w;
-		int var = q / 3;
+		int w = var1 - var2;
+		int q = 1 + w;
+		int var = q - 3;
 		if (q > var) { 
 			return 1;
 		}else {
@@ -15,7 +17,9 @@ public class SUT {
 	public static void main(String[] args) {
 		
 		int e = Math(1,2);
+		System.out.println(e);
 
 	}
 
 }
+
---
+++ FinalProject_29/src/main/java/FinalProject_29/MutTestingPIT.java
@@ -0,0 +1,56 @@
+package FinalProject_29;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileWriter;  
+//import java.io.*; 
+
+public class MutTestingPIT {
+	
+	public static void main(String[] args) throws IOException {
+		FileWriter fw=new FileWriter("C:\\Users\\Renata\\eclipse-workspace\\FinalProject_29\\src\\main\\java\\FinalProject_29/mutants.txt");   
+		BufferedReader reader;
+		int lineNumber = 0;
+		char[] mathOper = {'+','-','*','/'};
+		int[] mutantPerType = new int [4];
+		try {
+			reader = new BufferedReader(new FileReader(
+					//Please change the directory of your test file here
+					"C:\\Users\\Renata\\eclipse-workspace\\FinalProject_29\\src\\main\\java\\FinalProject_29/MathDummy.java"));
+			String line = reader.readLine();
+			while (line != null) {
+				for(int i = 0; i < mathOper.length; i++) {
+					int counter = line.indexOf(mathOper[i]);  
+					if(counter != -1) {
+//						fw.write("Original at line "+lineNumber+"\n"+mathOper[i]+" "+line+"\nMutants:\n"); 
+						fw.write("Original at line "+lineNumber+"\n"+line+"\nMutants:\n");
+						StringBuilder myName = new StringBuilder(line);
+						for(int j = 0; j < mathOper.length; j++) {
+							if(j!=i) {
+								myName.setCharAt(counter, mathOper[j]);
+//								fw.write(mathOper[j]+" "+myName+"\n");s
+								fw.write(myName+"\n");
+								mutantPerType[j]++;
+							}
+						}	
+//						fw.write("\n");
+					}
+				}
+				// read next line
+				lineNumber++;
+				line = reader.readLine();
+			}
+			fw.write("\n\n");
+			for(int i = 0; i < mathOper.length; i++) {
+				fw.write("Total mutants for "+mathOper[i]+" is "+mutantPerType[i]+"\n");
+			}
+			reader.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		fw.close();   
+	}
+
+}
